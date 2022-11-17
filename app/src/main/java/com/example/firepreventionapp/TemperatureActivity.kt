c38c72ac942b13473db49b765bfeb46473eb288b
johanx22x <johanrodsa2210@gmail.com>
2022-11-16 23:11:30 -0600

    Add a method to change the temperature in temperature activity

--- app/src/main/java/com/example/firepreventionapp/TemperatureActivity.kt
+++ app/src/main/java/com/example/firepreventionapp/TemperatureActivity.kt
@@ -7,5 +7,18 @@ class TemperatureActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_temperature)
+
+        updateActualTemperature(0)
+        updateMaxTemperature(0)
+    }
+
+    private fun updateActualTemperature(value : Int) {
+        val tempMsg = findViewById<TextView>(R.id.actual_temperature)
+        tempMsg.text = getString(R.string.temp_formatter, value)
+    }
+
+    private fun updateMaxTemperature(value : Int) {
+        val tempMsg = findViewById<TextView>(R.id.max_temperature)
+        tempMsg.text = getString(R.string.temp_formatter, value)
     }
 }
