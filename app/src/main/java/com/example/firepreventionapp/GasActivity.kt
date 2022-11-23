a806f513005c5d3f9e6e2b5f0c145e141b206a5c
Johanx22x <johanrodsa2210@gmail.com>
2022-11-23 01:56:11 -0600

    Add gas layout

--- app/src/main/java/com/example/firepreventionapp/GasActivity.kt
+++ app/src/main/java/com/example/firepreventionapp/GasActivity.kt
@@ -1,11 +1,27 @@
 package com.example.firepreventionapp
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.annotation.RequiresApi
 
 class GasActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gas)
+
+        updateActualGas(MainActivity.actualGas)
+
+        val refreshButton = findViewById<TextView>(R.id.gas_refresh)
+        refreshButton.setOnClickListener {
+            updateActualGas(MainActivity.actualGas)
+        }
+    }
+
+    private fun updateActualGas(value: String) {
+        val gasMsg = findViewById<TextView>(R.id.gas_value)
+        gasMsg.text = getString(R.string.gas_formatter, value)
     }
 }
